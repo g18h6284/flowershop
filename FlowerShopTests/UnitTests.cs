70a462fbfac2e618c51751ada3f9def97590f867
Liam Head <kento.nion2012@gmail.com>
2020-02-24 20:23:12 +0200

    Attempted to create the test for order class using mocks - NSubstitute

--- FlowerShopTests/UnitTests.cs
+++ FlowerShopTests/UnitTests.cs
@@ -1,4 +1,6 @@
 using NUnit.Framework;
+using NSubstitute;
+using FlowerShop;
 
 namespace Tests
 {
@@ -12,7 +14,16 @@ namespace Tests
         [Test]
         public void Test1()
         {
-            Assert.Pass();
+            //ARRANGE
+            anOrderDAO = Substitute.For<IOrderDAO>();
+            aClient = Substitute.For<IClient>();
+            anOrder = new Order(anOrderDAO, aClient);
+            //ACT
+            anOrder.Deliver();
+            //ASSERT
+            anOrderDAO.Recieved().SetDelivered();
+            anOrderDAO.NotRecieved().SetDelivered();
+            Assert.That();
         }
     }
 }
